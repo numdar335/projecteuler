c047aade04995b053fcd51bd8888e380102246d3
Nikos Karellas <56404005+numdar335@users.noreply.github.com>
2021-04-30 12:04:40 +0300

    Update 30.py

--- 30.py
+++ 30.py
@@ -1,11 +1,10 @@
-#task description: https://projecteuler.net/problem=30
+# task description: https://projecteuler.net/problem=30
 import math
 sum = 0
 for i in range(10,9*9*9*9*9*math.floor(1+math.log10(5)+5*math.log10(9))+1):
-  temp = 0
-  a = str(i)
-  for j in range (1,len(a)+1):
-    b = i//10**(j-1)%10
+  temp, a = 0, len(str(i))
+  for j in range(a):
+    b = i//10**j%10
     temp += b*b*b*b*b
   if i == temp: sum += temp
 print(sum)
